2a7ca42f2641c4550c1ec8883b02aae7141fe2f2
Dikshabharti06 <dikshabharti.db.06@gmail.com>
2025-05-27 18:05:23 +0530

    first commit

--- calc.js
+++ calc.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 function pick(val){
     document.getElementById("a").value +=val;
-}
-                
+}                
 function clr(){
     document.getElementById("a").value=" ";
 }
@@ -11,18 +9,4 @@ function solve(){
     var y=eval(x);
     document.getElementById("a").value=y;
 
-=======
-function pick(val){
-    document.getElementById("a").value +=val;
-}
-                
-function clr(){
-    document.getElementById("a").value=" ";
-}
-function solve(){
-    var x=document.getElementById("a").value;
-    var y=eval(x);
-    document.getElementById("a").value=y;
-
->>>>>>> b177c1eefd5dbe7d9bd1f66092dd6af4d8a53bac
 }
